330b462b83322d05431a3472de7a60d15eb80597
josemizael <josemisaelsampaio@gmail.com>
2025-03-25 19:05:05 +0000

    começo do cadastros das cartas

---
+++ nivel_novato.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+  //carta 1
+  char estado1;
+  char nome_da_cidade1;
+  unsigned int populacao1;
+  float area1;
+  long double pib1;
+  int pontos_turisticos1;
+  long double densidade_populacional1, pib_percapita2;
+  
+  // carta 2
+  char estado2;
+  char nome_da_cidade2;
+  unsigned int populacao2;
+  float area2;
+  long double pib2;
+  int pontos_turisticos2;
+  long double densidade_populacional2, pib_percapita2;
+
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
